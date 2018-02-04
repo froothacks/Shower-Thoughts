bfe84409fbafb2e19b8d456904e515a70c99b789
Ethan Guo <ethguo@gmail.com>
2018-02-03 20:10:16 -0500

    Better genius.py but getting rate limited

--- data-processing/genius.py
+++ data-processing/genius.py
@@ -1,6 +1,3 @@
-import sys
-print(sys.version)
- 
 import requests
 import pickle
 
@@ -8,39 +5,52 @@ geniusHeaders = {"Authorization": "Bearer J3n5zoDA27CGDoxTlLPsmZW8L_PONjoI8kQ_yW
 
 songAnnotations = []
 
+songIndex = 0
+runningTotal = 0
+
 with open("songs.txt") as f:
     songTitles = f.read().split("\n")
     for songTitle in songTitles:
-        print(songTitle, end=": ")
+        try:
+            songIndex += 1
+            print(songIndex, songTitle, end=": ")
 
-        # Get song id from title
-        params = {"q": songTitle}
-        res = requests.get("https://api.genius.com/search", headers=geniusHeaders, params=params)
-        json = res.json()
-        hits = json["response"]["hits"]
-        if len(hits) == 0:
-            print("Song not found")
-            continue
+            # Get song id from title
+            params = {"q": songTitle}
+            res = requests.get("https://api.genius.com/search", headers=geniusHeaders, params=params, timeout=1)
+            json = res.json()
+            hits = json["response"]["hits"]
+            if len(hits) == 0:
+                print("Song not found")
+                continue
 
-        songid = hits[0]["result"]["id"]
+            songid = hits[0]["result"]["id"]
 
-        # Get song annotations from song id
-        params = {"song_id": songid, "per_page": 50, "text_format": "plain"}
-        res = requests.get("https://api.genius.com/referents", headers=geniusHeaders, params=params)
-        json = res.json()
-        referents = json["response"]["referents"]
+            # Get song annotations from song id
+            params = {"song_id": songid, "per_page": 50, "text_format": "plain"}
+            res = requests.get("https://api.genius.com/referents", headers=geniusHeaders, params=params, timeout=1)
+            json = res.json()
+            referents = json["response"]["referents"]
 
-        count = 0
-        for referent in referents:
-            lyric = referent["fragment"]
-            annotation = referent["annotations"][0]["body"]["plain"]
-            votes = referent["annotations"][0]["votes_total"]
-            verified = referent["annotations"][0]["verified"]
-            if votes >= 40 or verified:
-                count += 1
-                songAnnotations.append((lyric, annotation))
+            count = 0
+            for referent in referents:
+                lyric = referent["fragment"]
+                annotation = referent["annotations"][0]["body"]["plain"]
+                votes = referent["annotations"][0]["votes_total"]
+                verified = referent["annotations"][0]["verified"]
+                if votes >= 40 or verified:
+                    count += 1
+                    runningTotal += 1
+                    songAnnotations.append((lyric, annotation))
 
-        print(count, "out of", len(referents))
+            print("%d out of %d (Total: %d)" % (count, len(referents), runningTotal))
+
+            if runningTotal >= 30000:
+                break
+
+        except Exception as e:
+            print(e)
+
 
 print(len(songAnnotations))
 
