eb18b3248378957cdd11a04ca5fa435e8083d976
Ethan Guo <ethguo@gmail.com>
2018-02-04 02:40:38 -0500

    Added more flasky

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ app = Flask(__name__)
 def get_index():
 	return "Hello World"
 
+@app.route("/results")
+def get_results():
+	
+	return "Hello World"
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
