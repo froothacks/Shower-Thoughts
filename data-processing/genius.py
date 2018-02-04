a14864211c06f3262f5cb266a8bb7b32a7f50cce
Ethan Guo <ethguo@gmail.com>
2018-02-03 19:35:27 -0500

    Retrieved annotations from top 100 songs

---
+++ data-processing/genius.py
@@ -0,0 +1,45 @@
+import sys
+print(sys.version)
+ 
+import requests
+import pickle
+
+geniusHeaders = {"Authorization": "Bearer J3n5zoDA27CGDoxTlLPsmZW8L_PONjoI8kQ_yWVJkPvpy1ea4Ya8wOSJcG1Bj6bl"}
+
+songAnnotations = []
+
+with open("songs.txt") as f:
+    songTitles = f.read().split("\n")
+    for songTitle in songTitles:
+        # Get song id from title
+        params = {"q": songTitle}
+        res = requests.get("https://api.genius.com/search", headers=geniusHeaders, params=params)
+        json = res.json()
+        songid = json["response"]["hits"][0]["result"]["id"]
+
+        # Get song annotations from song id
+        params = {"song_id": songid, "per_page": 50, "text_format": "plain"}
+        res = requests.get("https://api.genius.com/referents", headers=geniusHeaders, params=params)
+        json = res.json()
+        referents = json["response"]["referents"]
+        print(songTitle, end=": ")
+
+        count = 0
+        for referent in referents:
+            lyric = referent["fragment"]
+            annotation = referent["annotations"][0]["body"]["plain"]
+            votes = referent["annotations"][0]["votes_total"]
+            verified = referent["annotations"][0]["verified"]
+            if votes >= 40 or verified:
+                count += 1
+                songAnnotations.append((lyric, annotation))
+
+        print(count, end=" out of ")
+        print(len(referents))
+
+print(len(songAnnotations))
+
+with open("annotations.dat", "wb") as f:
+    pickle.dump(songAnnotations, f)
+
+print("Done!")
